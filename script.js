8f8923da1665f454a114ed67bcc9395b019b779d
viniciuswx <vinnicorrea7@gmail.com>
2024-08-27 16:19:14 -0300

    exclusão

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
-});
